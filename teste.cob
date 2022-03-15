790d00d820a335cbf42efa018bf7d99ec1634610
AndersonANascimento <anderson.a.nascimento@gmail.com>
2022-03-15 15:12:09 -0400

    Aula 001

---
+++ teste.cob
@@ -0,0 +1,23 @@
+      *PROGRAMA DE TESTE
+      *************************
+       IDENTIFICATION DIVISION.
+      *************************
+           PROGRAM-ID. TESTE.
+
+      **********************
+       ENVIRONMENT DIVISION.
+      **********************
+      
+      ***************
+       DATA DIVISION.
+      ***************
+         WORKING-STORAGE SECTION.
+           01 WS-NOME        PIC X(10).
+
+      ********************
+       PROCEDURE DIVISION.
+      ********************
+           DISPLAY "Digite um nome: "
+           ACCEPT WS-NOME.
+           DISPLAY "O nome digitado foi: " WS-NOME
+       STOP RUN.
