09d67aa04ec466eddb1679061942970139e48fa7
AndersonANascimento <anderson.a.nascimento@gmail.com>
2022-03-23 13:58:13 -0400

    Inclusão de condicional e campos maskarados.

--- curso/src/PGEXE001.cbl
+++ curso/src/PGEXE001.cbl
@@ -16,25 +16,33 @@
        FILE SECTION.
 
        WORKING-STORAGE SECTION.
-       01 WS-NUMERO-1                          PIC 9(003) VALUE ZEROS.
-       01 WS-NUMERO-2                          PIC 9(003) VALUE ZEROS.
-       01 WS-RESULTADO                         PIC Z(004) VALUE ZEROS.
+       01 WS-NUMERO-1                          PIC 9(03) VALUE ZEROS.   ANDERSON
+       01 WS-NUMERO-2                          PIC 9(03) VALUE ZEROS.
+       01 WS-RESULTADO-Z                       PIC Z(04) VALUE ZEROS.
+       01 WS-LIMITE-Z                          PIC Z(04) VALUE 10.
 
       ********************
        PROCEDURE DIVISION.
       ********************
+      *    Este é um paragrafo
        MAIN-PROCEDURE.
-           DISPLAY "Exibe o resultado da soma de 2 números aleatorios".
-           
-           DISPLAY "Digite o 1º número: ".
+           DISPLAY 'Exibe o resultado da soma de 2 números aleatorios'.
+
+           DISPLAY 'Digite o 1º número: '.
            ACCEPT WS-NUMERO-1.
            
-           DISPLAY "Digite o 2º número: ".
+           DISPLAY 'Digite o 2º número: '.
            ACCEPT WS-NUMERO-2.
 
-           COMPUTE WS-RESULTADO = WS-NUMERO-1 + WS-NUMERO-2.
+           COMPUTE WS-RESULTADO-Z = WS-NUMERO-1 + WS-NUMERO-2.
 
-           DISPLAY "O resultado é: " WS-RESULTADO.
+           IF WS-RESULTADO-Z  > WS-LIMITE-Z THEN  
+               DISPLAY 'O resultado é maior que ' WS-LIMITE-Z 
+           ELSE
+               DISPLAY 'O resultado é menor que ' WS-LIMITE-Z 
+           END-IF.
+           
+           DISPLAY 'O resultado é: ' WS-RESULTADO-Z.
 
            STOP RUN.
        END PROGRAM PGEXE001.
